a99eb031be93ce577d5bdc3589288b322e6e63c2
deraolisah <deraolisah@gmail.com>
2025-08-29 21:55:52 +0100

    Update: Toggle Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,21 @@ const Navbar = () => {
     <nav className='w-full pb-4 flex flex-wrap items-center justify-between border-b border-gray-300'>
       <a href='/' className='text-lg font-bold'> Meco Club </a>
 
-      <div className='flex md:hidden flex-col gap-1 cursor-pointer' onClick={toggleMenu} title='menu'>
-        <span className='h-1 w-6 bg-black rounded-full'></span>
-        <span className='h-1 w-6 bg-black rounded-full'></span>
+      <div className='flex md:hidden cursor-pointer' onClick={toggleMenu} title='menu'>
+        {!menuOpen ? (
+          <div className='flex flex-col gap-1'>
+            <span className='h-0.5 w-6 bg-black rounded-full'></span>
+            <span className='h-0.5 w-6 bg-black rounded-full'></span>
+          </div>
+        ) : (
+          <div className='text-5xl w-4 h-4 flex items-center justify-center font-normal'>
+            ×
+          </div>
+        )}
       </div>
 
-      <ul className={`md:flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-6 font-semibold w-full md:w-fit py-2 md:py-0 ${menuOpen ? "flex" : "hidden"}`}>
+
+      <ul className={`md:max-h-fit flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-6 font-semibold w-full md:w-fit overflow-hidden transition-all duration-200 ${menuOpen ? "max-h-auto" : "max-h-0"}`}>
         <li><a href="/releases" className='hover:text-gray-400'> Releases </a></li>
         <li><a href="/history" className='hover:text-gray-400'> History </a></li>
         <li><a href="/store" className='hover:text-gray-400'> Store </a></li>
